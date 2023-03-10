3d1ec4780e81f9a27cfad6f39de8195f5fcf021a
Andrey Klevtsov <andrewklevt@gmail.com>
2023-03-10 17:29:39 +0300

    Exceptions names edit

--- exceptions.py
+++ exceptions.py
@@ -1,12 +1,12 @@
 class SendMessageError(Exception):
-    """Ошибка при отправке сообщения в телеграмм"""
+    """Ошибка при отправке сообщения в телеграмм."""
 
     def __init__(self, *args, **kwargs):
         pass
 
 
 class ApiStatusError(Exception):
-    """Ошибка при запросе к API"""
+    """Ошибка при запросе к API."""
 
     def __init__(self, *args, **kwargs):
         pass
